1d00d31b00e908f80a5652d771d3b3e523154335
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-04-08 10:17:57 +0900

    create function add memo to list

--- js/api.js
+++ js/api.js
@@ -8,6 +8,23 @@ const api = {
       alert('error searching memos')
       throw error
     }
+  },
+
+    async saveMemos(memo) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify()
+      })
+      return await response.json(memo)
+    }
+    catch {
+      alert('error save memo')
+      throw error
+    }
   }
 }
 
--- js/ui.js
+++ js/ui.js
@@ -6,21 +6,37 @@ const ui = {
 
     try {
       const memos = await api.searchMemos()
-      memos.forEach(memo => {
-        memoList.innerHTML += `
-          <li class="li-pensamento" data-id="${memo.id}">
-          <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-          <div class="pensamento-conteudo">${memo.conteudo}</div>
-          <div class="pensamento-autoria">${memo.autoria}</div>
-          </li>
-        `
-      });
+      memos.forEach(ui.addMemoToList)
     }
     catch {
       alert('Error rendering memos')
     }
-  }
+  },
 
+  addMemoToList() {
+    const memoList = document.getElementById('lista-pensamentos')
+    const li = document.createElement("li")
+    li.setAttribute("data-id", memo.id)
+    li.classList.add("li-pensamento")
+
+    const iconMarks = document.createElement("img")
+    iconMarks.src = "assets/imagens/aspas-azuis.png"
+    iconMarks.alt = "Aspas azuis"
+    iconMarks.classList.add("icone-aspas")
+
+    const memoContent = document.createElement("div")
+    memoContent.textContent = memo.conteudo
+    memoContent.classList.add("pensamento-conteudo")
+
+    const memoAuthor = document.createElement("div")
+    memoAuthor.textContent = memo.autoria
+    memoAuthor.classList.add("pensamento-autoria")
+
+    li.appendChild(iconMarks)
+    li.appendChild(memoContent)
+    li.appendChild(memoAuthor)
+    memoList.appendChild(li)
+  }
 }
 
 export default ui;
